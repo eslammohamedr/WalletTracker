7a110a6c8d938155037cfc678c85d1b3cb68a611
Eslam Mohamed <86850708+eslammohamedr@users.noreply.github.com>
2026-04-28 23:06:01 +0300

    Enhance logic4

---
+++ app/src/main/java/com/example/wallettrackers/model/CategoryRule.kt
@@ -0,0 +1,8 @@
+package com.example.wallettrackers.model
+
+data class CategoryRule(
+    val id: String = "",
+    val merchantKeyword: String = "",
+    val category: String = "",
+    val userId: String = ""
+)
